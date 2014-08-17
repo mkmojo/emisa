3c38b2d9df08d5c9deb3af3238734495d6a5a1d0
Qiyuan Qiu <qiuqiyuan@gmail.com>
2014-08-17 15:45:29 -0400

    change code to plot each parent cite over time.

--- data_process.py
+++ data_process.py
@@ -3,6 +3,7 @@ import ast
 import sys
 import matplotlib.pyplot as plt
 
+
 def save(path, ext='png', close=True, verbose=True):
     """Save a figure from pyplot.
 
@@ -56,7 +57,6 @@ def save(path, ext='png', close=True, verbose=True):
         print("Done")
 
 
-
 def parse_inputfile(input_filename):
     path = os.path.join(os.getcwd(), input_filename)
     fhand = open(path)
@@ -87,7 +87,7 @@ def draw_chart(intput_list, dist_filename, prefix):
         x.append(item[0])
         y.append(item[1])
     plt.plot(x, y, 'o')
-    plt.axis([min(x) - 10, max(x) + 10, min(y), max(y) + 0.1])
+    plt.axis([min(x) - 10, max(x) + 10, 0, max(y) + 0.1])
     save(os.path.join(prefix, dist_filename), ext='png', close=True, verbose=True)
     plt.close()
 
@@ -112,7 +112,6 @@ def save_distribution(year, dist, dist_filename, prefix):
 
 
 def do_data_process(wanted_journal_ids):
-
     num_totals = []
     num_wanteds = []
     num_citations = []
@@ -136,7 +135,7 @@ def do_data_process(wanted_journal_ids):
         for line in fhand:
             #skip first line
             if line.startswith("year"): continue
-            num_total = num_total + 1
+            num_total += 1
 
             #extract counter ends
             atpos = line.find("Counter")
@@ -148,6 +147,7 @@ def do_data_process(wanted_journal_ids):
                                         line[counter_start:counter_end]
 
             #Ignore those that are not listed papers
+            #Chose eco_papers
             if journal_id not in wanted_journal_ids: continue
 
             counter = ast.literal_eval(counter)
@@ -158,20 +158,19 @@ def do_data_process(wanted_journal_ids):
             for key, val in counter.items():
                 children_paper_counts[key] = children_paper_counts.get(key, 0) + val
 
-            num_wanted = num_wanted + 1
+            num_wanted += 1
         try:
             fhand.close()
         except:
             print "Close fail:", file_name
 
-
         #total number of citation for that year
-        num_citation = sum(children_paper_counts.values())
+        num_children_total_citation = sum(children_paper_counts.values())
         for key, val in children_paper_counts.items():
-            distribution[key] = float(val) / num_citation
+            distribution[key] = float(val) / num_wanted
 
         print "num_uniq_child_paper_cite_year:", len(children_paper_counts)
-        print "num_total_child_citation:", num_citation
+        print "num_total_child_citation:", num_children_total_citation
         print "num_wanted_paper:", num_wanted
         print "num_total_paper:", num_total
         print ""
@@ -179,8 +178,9 @@ def do_data_process(wanted_journal_ids):
         distributions.append((int(year), distribution))
         num_totals.append((int(year), num_total))
         num_wanteds.append((int(year), num_wanted))
-        num_citations.append((int(year), num_citation))
+        num_citations.append((int(year), num_children_total_citation))
     return distributions, num_totals, num_wanteds, num_citations
+
 
 def save_num_totals(prefix, filename, num_totals):
     file_path = os.path.join(os.getcwd(), prefix + "/src", filename)
@@ -193,6 +193,7 @@ def save_num_totals(prefix, filename, num_totals):
             val = item[1]
             fhand.write(str(year) + " " + str(val) + "\n")
     draw_chart(num_totals, filename, prefix + "/images")
+
 
 def save_num_wanteds(prefix, filename, num_totals):
     file_path = os.path.join(os.getcwd(), prefix + "/src", filename)
@@ -219,6 +220,7 @@ def save_num_citations(prefix, filename, num_totals):
             fhand.write(str(year) + " " + str(val) + "\n")
     draw_chart(num_totals, filename, prefix + "/images")
 
+
 def save_num_ratio(prefix, filename, num_totals):
     file_path = os.path.join(os.getcwd(), prefix + "/src", filename)
     if not os.path.exists(os.path.dirname(file_path)):
@@ -231,6 +233,7 @@ def save_num_ratio(prefix, filename, num_totals):
             fhand.write(str(year) + " " + str(val) + "\n")
     draw_chart(num_totals, filename, prefix + "/images")
 
+
 def calculate_ratio(num_wanteds, num_totals):
     assert len(num_wanteds) == len(num_totals)
     num_wanteds.sort()
@@ -238,8 +241,57 @@ def calculate_ratio(num_wanteds, num_totals):
     ratio = []
     for i in xrange(len(num_wanteds)):
         assert num_wanteds[i][0] == num_totals[i][0]
-        ratio.append((num_wanteds[i][0] ,float(num_wanteds[i][1]) / num_totals[i][1]))
+        ratio.append((num_wanteds[i][0], float(num_wanteds[i][1]) / num_totals[i][1]))
     return ratio
+
+
+def save_total_cites_per_paper_over_time(prefix, wanted_journal_ids):
+    files = os.listdir(os.getcwd())
+    for file_name in files:
+        if not (file_name[len(file_name) - 4:] == ".txt" and file_name.startswith("cite")): continue
+
+        with open(file_name, 'r') as fhand:
+            for line in fhand:
+
+                if line.startswith("year"): continue
+
+                #extract counter ends
+                atpos = line.find("Counter")
+                line_fst_hlf = line[:atpos].split()
+                counter_start = atpos + len("Counter") + 1
+                counter_end = line.find(")", counter_start)
+
+                year, journal_id, parent_paper_id, counter = line_fst_hlf[0], line_fst_hlf[1], \
+                                                             line[(atpos - 16):(atpos - 1)],  \
+                                                             line[counter_start:counter_end]
+
+                #Ignore those that are not listed papers
+                #Chose eco_papers
+                if journal_id not in wanted_journal_ids: continue
+
+                counter = ast.literal_eval(counter)
+                #pretend that data point does not even show up in the dataset
+                if "NULL" in counter.keys(): continue
+
+                #save graph to disk
+                lst = []
+                for key, val in counter.items():
+                    lst.append((int(key), val))
+                lst.sort()
+                draw_chart(lst, parent_paper_id, prefix + "/images" +"/" + file_name)
+
+
+                #save src file to disk
+                file_path = os.path.join(os.getcwd(), prefix + "/src", file_name, parent_paper_id)
+                if not os.path.exists(os.path.dirname(file_path)):
+                    os.makedirs(os.path.dirname(file_path))
+                with open(file_path, 'w') as out_fhand:
+                    for item in lst:
+                        x = item[0]
+                        y = item[1]
+                        out_fhand.write(str(x) + ' ' + str(y) + '\n')
+
+
 
 def main():
     if len(sys.argv) < 2:
@@ -260,23 +312,23 @@ def main():
         year = item[0]
         distribution = item[1]
         dist_filename = "dist_" + str(year) + ".dist"
-        save_distribution(year, distribution, dist_filename, "results/dist")
-
-    #save total number of papers each year to file
-    save_num_totals(prefix = "results/num_total_papers", filename = \
-        "total_paper.count", num_totals=num_totals)
-
-    #save total number of eco papers each year to file
-    save_num_wanteds(prefix = "results/num_eco_papers", filename = \
-        "total_eco_citation.count", num_totals=num_wanteds)
-
-    #save total number of citations each year to file
-    save_num_citations(prefix = "results/num_citations", filename = \
-        "total_citation.count", num_totals=num_citations)
+        save_distribution(year, distribution, dist_filename, "results/step1")
 
     #save ratio of total_eco_paper/totol_all_paper to file
-    save_num_ratio(prefix = "results/ratio", filename = \
-        "ratio.count", num_totals=ratios)
+    save_num_ratio(prefix="results/step2", filename= \
+        "ratio.txt", num_totals=ratios)
+
+    #save total number parents papers each year to file
+    save_num_totals(prefix="results/step3", filename= \
+        "total_paper.txt", num_totals=num_totals)
+
+    #save total cites per parent paper over time
+    save_total_cites_per_paper_over_time(prefix="results/step4", wanted_journal_ids=wanted_journal_ids)
+
+    #save total number of citations each year to file
+    save_num_citations(prefix="results/step5", filename= \
+        "total_citation.txt", num_totals=num_citations)
+
 
 if __name__ == "__main__":
     main()
